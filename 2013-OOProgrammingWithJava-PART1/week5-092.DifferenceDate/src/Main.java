efe601a441da83369746ef558f935c2d2fa07fb8
mark757 <macunan90@gmail.com>
2016-12-04 16:18:29 -0500

    week 5, 92 complete

--- 2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/Main.java
+++ 2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/Main.java
@@ -3,5 +3,25 @@ public class Main {
 
     public static void main(String[] args) {
         // write testcode here
+        MyDate first = new MyDate(24, 12, 2009);
+        MyDate second = new MyDate(1, 1, 2011);
+        MyDate third = new MyDate(25, 12, 2010);
+
+        MyDate fourth = new MyDate(9, 3, 2012);
+        MyDate fifth = new MyDate(10, 3, 2011);
+
+        System.out.println (fifth + " and " + fourth + " difference in years: " + fifth.differenceInYears(fourth) );
+        System.out.println('\n');
+        System.out.println( first + " and " + second + " difference in years: " + second.differenceInYears(first) );
+        System.out.println('\n');
+        System.out.println( second + " and " + first + " difference in years: " + first.differenceInYears(second) );
+        System.out.println('\n');
+        System.out.println( first + " and " + third + " difference in years: " + third.differenceInYears(first) );
+        System.out.println('\n');
+        System.out.println( third + " and " + first + " difference in years: " + first.differenceInYears(third) );
+        System.out.println('\n');
+        System.out.println( third + " and " + second + " difference in years: " + second.differenceInYears(third) );
+        System.out.println('\n');
+        System.out.println( second + " and " + third + " difference in years: " + third.differenceInYears(second) );
     }
 }
--- 2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/MyDate.java
+++ 2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/MyDate.java
@@ -1,10 +1,14 @@
-public class MyDate {
+import java.util.Calendar;
+
+import static java.lang.Math.abs;
+
+public class MyDate { //declaring member variables
     private int day;
     private int month;
     private int year;
 
     public MyDate(int day, int montd, int year) {
-        this.day = day;
+        this.day = day;  //constructor class with day, month, year parameters
         this.month = montd;
         this.year = year;
     }
@@ -30,4 +34,50 @@ public class MyDate {
         return false;
     }
 
+    public int differenceInYears(MyDate comparedDate) {
+
+        int years = abs(comparedDate.year - this.year);
+
+        int months;
+        int days = abs(comparedDate.day - this.day);
+       // int MAX_MONTHS = 12;
+       // int MAX_DAYS_IN_YEAR = 365;
+        int MAX_DAY_IN_MONTH = 30;
+
+        if (!earlier(comparedDate)) {
+
+            if (comparedDate.month > this.month || comparedDate.day > this.day) {
+                months = 0;
+                years--;
+
+            } else {
+                months = abs(comparedDate.month - this.month);
+
+            }
+
+            if (comparedDate.day > this.day) {
+                days = (MAX_DAY_IN_MONTH - comparedDate.day) + this.day;
+            }
+
+        }
+
+        if (earlier(comparedDate)) {
+            if (comparedDate.month < this.month || comparedDate.day < this.day) {
+                months = 0;
+                years--;
+
+            } else {
+                months = abs(comparedDate.month - this.month);
+            }
+
+           // System.out.println("years: " + years);
+
+            //System.out.println("months: " + months);
+
+           // System.out.println("days: " + days);
+        }
+            return years;
+        }
+
 }
+
